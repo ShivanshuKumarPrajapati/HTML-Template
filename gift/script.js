352e6870da9b6c7dbb3eba544fea65b34af5a904
ShivanshuKumarPrajapati <shivanshu.rkt@gmail.com>
2022-06-28 20:51:26 +0530

    implement card carousel completely and footer basic layout

--- gift/script.js
+++ gift/script.js
@@ -5,17 +5,22 @@ $(document).ready(function(){
     var swiper = new Swiper(".mySwiper", {
         breakpoints:{
             0:{
-                slidesPerView: 3,
+                slidesPerView: 1,
+            },
+            350:{
+                slidesPerView:2
+            },
+            630:{
+                slidesPerView:3
             },
             750:{
                 slidesPerView: 4,
                 spaceBetween:0,
             },
-            1100:{
+            1110:{
                 slidesPerView: 5,
             },
         },
-    
         slidesPerGroup: 1,
         loop: true,
         loopFillGroupWithBlank: true,
@@ -25,6 +30,12 @@ $(document).ready(function(){
           prevEl: ".swiper-button-prev"
         }
       });
+      $(window).resize(function(){
+        if(window.innerWidth < 600){
+            $('.AddBtn').text('Add');
+        }  
+      })
+
 
 })
 
