35761380ff93d520ea90a3f382e15f34e14327a6
ShivanshuKumarPrajapati <shivanshu.rkt@gmail.com>
2022-06-28 10:39:50 +0530

    implement first carousel using swiper

--- gift/script.js
+++ gift/script.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
     $('.sidenav').sidenav(); 
     $(".dropdown-trigger").dropdown({hover: false} );
-    $('.carousel').carousel(); 
+    
+    var swiper = new Swiper(".mySwiper", {
+        breakpoints:{
+            0:{
+                slidesPerView: 3,
+            },
+            750:{
+                slidesPerView: 4,
+                spaceBetween:0,
+            },
+            1100:{
+                slidesPerView: 5,
+            },
+        },
+    
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      });
+
 })
 
